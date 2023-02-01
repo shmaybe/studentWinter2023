4fa10eca1d19f12eaf862501b73d9b3efd60181d
MayxT <MayxT@10.16.65.209>
2023-02-01 16:32:05 -0500

    commit

---
+++ src/student/Student.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author MayxT
+ */
+public class Student {
+
+    private String studentID;
+    private String studentName;
+
+    public Student(String studentID, String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+}
